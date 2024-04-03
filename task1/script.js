032dc20d2a538b3b1c0b501676de3cac842f6161
Timur Danilov <timur.danilov.1985@mail.ru>
2024-04-03 04:50:29 +0300

    решение домашнего задания 1

---
+++ task1/script.js
@@ -0,0 +1,8 @@
+let celsius = 20;
+let fahrenheit = celsius * 1.8 + 32;
+console.log(`Температура по Цельсию: ${celsius} градусов C`);
+console.log(`Температура по Фаренгейту: ${fahrenheit} градусов F`);
+
+let firstName = 'Тимур';
+let admin = firstName;
+console.log(admin);
